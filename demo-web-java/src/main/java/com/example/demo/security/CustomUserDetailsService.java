c9ea29f9d30f86c9a5fed33062cfe5b4bdeeb350
jhkim105 <jihwan.kim@rsupport.com>
2017-11-29 13:30:13 +0900

    demo-web-java redis 추가

--- demo-web-java/src/main/java/com/example/demo/security/CustomUserDetailsService.java
+++ demo-web-java/src/main/java/com/example/demo/security/CustomUserDetailsService.java
@@ -1,19 +1,28 @@
 package com.example.demo.security;
 
-import com.example.demo.repository.UserRepository;
 import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.servlet.http.HttpSession;
+
+@Slf4j
 public class CustomUserDetailsService implements UserDetailsService {
   @Autowired
   private UserRepository userRepository;
+
+  @Autowired
+  HttpSession httpSession;
+
   @Override
   @Transactional(readOnly = true)
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    log.debug("{}", httpSession.getId());
     User user = userRepository.findByUsername(username);
     return new CustomUserDetails(user);
   }
